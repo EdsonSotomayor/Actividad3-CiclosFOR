b74494dce67282c28191672b6807e1e0fd1afac7
Edson Sotomayor <esotomayor0@ucol.mx>
2019-08-22 13:18:39 -0500

    v1

---
+++ Ciclos FOR/Ciclos FOR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ciclos_FOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int x, y, z;
+            Console.WriteLine("NUMEROS DE MAYOR A MENOR Y DE MENOR A MAYOR");
+            Console.WriteLine("INDICA EL PRIMER NUMERO: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("INDICA EL SEGUNDO NUMERO: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("SUCECION DE NUMEROS: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                for (int i = x; i > y; i -= z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            else
+            {
+                for (int i = x; i < y; i += z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+
+        }
+    }
+}
